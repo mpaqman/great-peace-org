aeada69d056f645a1ad188f6b9af7f26d4c5cfda
Merrick Paq-Man <merrick.paqman@gmail.com>
2022-10-26 15:24:51 -0400

    Progress toward adding netifly form handling

---
+++ handle-form.js
@@ -0,0 +1,26 @@
+
+const processForm = form => {
+    const data = new FormData(form)
+    data.append('form-name', 'newsletter');
+    fetch('/', {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      form.innerHTML = `<div class="form--success">Almost there! Check your inbox for a confirmation e-mail.</div>`;
+    })
+    .catch(error => {
+      form.innerHTML = `<div class="form--error">Error: ${error}</div>`;
+    })
+  }
+
+  
+  const emailForm = document.querySelector('.email-form');
+  if (emailForm) {
+    console.log(emailForm)
+    emailForm.addEventListener('submit', e => {
+      e.preventDefault();
+      console.log("email! email! email!");
+      processForm(emailForm);
+    })
+  }
