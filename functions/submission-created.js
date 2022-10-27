f20d8247ae7eb78ed80b587dd40f78ff83e00623
Merrick Paq-Man <merrick.paqman@gmail.com>
2022-10-26 20:31:45 -0400

    Added script to log forum submissions

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,5 +1,9 @@
 module.exports.handler = async event => {
-    const email = JSON.parse(event.body).payload.email;
-    console.log(`Recieved a submission: ${email}`);
-    console.log(event);
+    const payload = JSON.parse(event.body).payload;
+    const email = payload.data.email;
+    const firstname = payload.data.firstname;
+    const lastname = payload.data.lastname;
+    //console.log(payload)
+    console.log(`Recieved a submission: ${email} ${firstname} ${lastname}`);
+
 }
